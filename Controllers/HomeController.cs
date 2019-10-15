88173c62679911152856f6d84bb1ac59fce20d6f
Luke DeBaltzo <50882363+lukedebaltzo@users.noreply.github.com>
2019-10-14 21:09:54 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Data_Structure_HW.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+    }
+}
